da4d1e71dea754130f5b0fbd64c731e08ae3a11d
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-09-27 18:25:24 -0400

    Created main.py
    - Created array to hold seconds and current ETH sum
    - Connected to Coinbase websocket
    - Calculated sum of ETH per second
    - Changed pixel color based on ETH sum

---
+++ main.py
@@ -0,0 +1,39 @@
+import json, numpy
+from sense_hat import SenseHat
+from websocket import create_connection
+from format_datetime import Format_Datetime
+from pixel import Pixel
+
+def Initialize_Array():
+    return numpy.array([[i, 0] for i in range(0, 60)])
+
+Sense_Hat = SenseHat()
+Sense_Hat_Pixel = Pixel()
+Datetime = Format_Datetime()
+
+Seconds = Initialize_Array()
+Sense_Hat.clear()
+
+Coinbase_WebSocket = create_connection("wss://ws-feed.exchange.coinbase.com")
+Coinbase_WebSocket.send(json.dumps({"type": "subscribe", "product_ids": ["ETH-USD"], "channels": ["matches"]}))
+
+while True:
+
+    ETH_Data = json.loads(Coinbase_WebSocket.recv())
+
+    if "match" in ETH_Data["type"]:
+        if (Datetime.Current_Time.second == 0):
+            Sense_Hat.clear(0, 0, 0)
+            Seconds = Initialize_Array()
+
+        Index = Datetime.Current_Time.second
+        Sum = Seconds[Index][1]
+        Sum += float(ETH_Data["size"])
+
+        Row = Index % 8
+        Column = Index // 8
+
+        Sense_Hat_Pixel.Pixel_Color = Sum
+        Sense_Hat.set_pixel(Row, Column, Sense_Hat_Pixel.Pixel_Color)
+    else:
+        pass
