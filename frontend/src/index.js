cac73d7a6d0eb8464e099bfafd114373803c5c90
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-10-15 07:18:01 -0400

    Wrapped app inside query client

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+const Query_Client = new QueryClient();
+
 root.render(
     <React.StrictMode>
-        <App />
+        <QueryClientProvider client={Query_Client}>
+            <App />
+        </QueryClientProvider>
     </React.StrictMode>
 )
