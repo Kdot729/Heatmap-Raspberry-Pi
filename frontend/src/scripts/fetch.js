c6850b03e318d7a81871963aa8a413822dd78555
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-11-03 12:53:41 -0500

    Changed fetch interval

--- frontend/src/scripts/fetch.js
+++ frontend/src/scripts/fetch.js
@@ -5,12 +5,11 @@ export const useFetch = () =>
     {
         const Get_Request = async () =>
         {
-            //Need to change url because external IP always changes when launching instance
             const {data} = await axios.get(`http://127.0.0.1:8000`)
             return await data
         }
 
-        const {data, status} = useQuery({queryKey: [], queryFn: () => Get_Request(), refetchInterval: 5000})
+        const {data, status} = useQuery({queryKey: [], queryFn: () => Get_Request(), refetchInterval: 1})
 
         return status !== "success" ? [] : data
     }
