99956971ead361d717ec35f3ed3cf4e21fcb5f51
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-11-17 11:04:55 -0500

    Updated socket - Changed mongo connection - Removed CPU condition

--- socket/main.py
+++ socket/main.py
@@ -1,17 +1,12 @@
-import tracemalloc
 from websocket import create_connection
-import json, pymongo
+import json, pymongo, os
 from datetime import datetime
 
+Mongo_URI = os.getenv('MONGO_URI')
 
-Mongo_Client = pymongo.MongoClient(host='coinbase_mongodb',
-                        port=27017, 
-                        username='root', 
-                        password='example',
-                        authSource="admin")
-MongoDB = Mongo_Client["coinbase"]
-
-ETH_Collection = MongoDB["ETH"]
+Mongo_Client = pymongo.MongoClient(Mongo_URI)
+Mongo_Database = Mongo_Client["coinbase"]
+ETH_Collection = Mongo_Database["ETH"]
 
 Socket = create_connection("wss://ws-feed.exchange.coinbase.com")
 Socket.send(
@@ -25,18 +20,13 @@ Socket.send(
 )
 
 while True:
-        Memory_Usage = tracemalloc.get_traced_memory()
-        if Memory_Usage[0] >= 300000:
-            pass
-        else:              
-            ETH_Data = json.loads(Socket.recv())
+    ETH_Data = json.loads(Socket.recv())
 
-            if "match" in ETH_Data["type"]:
-                Data =  {
-                            "datetime": datetime.strptime(ETH_Data["time"], "%Y-%m-%dT%H:%M:%S.%fZ"),
-                            "side": ETH_Data["side"],
-                            "size": float(ETH_Data["size"]),
-                            "price": float(ETH_Data["price"])
-                        }
-
-                ETH_Collection.insert_one(Data)
+    if "match" in ETH_Data["type"]:
+        Data =  {
+                    "datetime": datetime.strptime(ETH_Data["time"], "%Y-%m-%dT%H:%M:%S.%fZ"),
+                    "side": ETH_Data["side"],
+                    "size": float(ETH_Data["size"]),
+                    "price": float(ETH_Data["price"])
+                }
+        ETH_Collection.insert_one(Data)
