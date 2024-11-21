a0f4f4a13f53ae17d7bcae11b336c79df567124c
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-11-21 09:14:49 -0500

    Added return for api

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
+from read import Read_Data
 
 app = FastAPI()
 
@@ -15,4 +16,4 @@ Router = APIRouter()
 
 @app.get("/")
 def table():
-    pass
+    return {"Data": Read_Data()}
